ff616727bde65a98e481f061cc227a3832e4ba73
Leslie Torres <Leslietorresc@gmail.com>
2023-07-11 04:20:03 -0400

    commit 3 devise views +atribu scaff comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :publications
-  get 'homes/index'
-  devise_for :users
+  devise_for :users, controller:{
+    sessiones: "users/sessions",
+    registrations: "users/registrations"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'homes/index'
   # Defines the root path route ("/")
    root "homes#index"
    get "/publications",  to: "publications#index", as:"user_root"
